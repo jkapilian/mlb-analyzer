4afd28d93e61839f33026f7e07535189c90782f4
Joshua Kapilian <kapgamesandapps2@gmail.com>
2024-10-22 22:28:24 -0700

    Added summary print

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import statsapi
 import argparse
 import csv
 import constants
-import json
 
 def build_analysis(args):
 	finalObj = {
@@ -15,10 +14,12 @@ def build_analysis(args):
 
 	with open(args.file) as csvfile:
 		reader = csv.reader(csvfile)
+		games = 0
 		for row in reader:
+			games += 1
 			gameId = get_game_id(row)
 			add_game(gameId, finalObj)
-		print(finalObj)
+		process_final(finalObj, games)
 
 def get_game_id(row):
 	home_team = row[0]
@@ -45,19 +46,22 @@ def add_game(gameId, finalObj):
 	for player in boxscore["away"]["players"]:
 		playerObj = boxscore["away"]["players"][player]
 		process_player(playerObj, finalObj)
-	finalObj["attendance"][gameId] = get_field(boxscore, "Att")
-	finalObj["gameTimes"][gameId] = get_field(boxscore, "T")
+	finalObj["attendance"][gameId] = int(get_field(boxscore, "Att").replace(",", ""))
+	time = get_field(boxscore, "T").split(":")
+	finalObj["gameTimes"][gameId] = 60 * int(time[0]) + int(time[1])
 	
 
 def process_player(playerObj, finalObj):
 	id = playerObj["person"]["id"]
+	played = playerObj["stats"]["batting"] != {} or playerObj["stats"]["pitching"]
 	homeRuns = playerObj["stats"]["batting"]["homeRuns"] if "homeRuns" in playerObj["stats"]["batting"] else 0
 	triples = playerObj["stats"]["batting"]["triples"] if "triples" in playerObj["stats"]["batting"] else 0
 	
-	if id in finalObj["players"]:
-		finalObj["players"][id] += 1
-	else:
-		finalObj["players"][id] = 1
+	if played:
+		if id in finalObj["players"]:
+			finalObj["players"][id] += 1
+		else:
+			finalObj["players"][id] = 1
 
 	if homeRuns > 0:
 		if id in finalObj["homeRuns"]:
@@ -74,8 +78,49 @@ def process_player(playerObj, finalObj):
 def get_field(boxscore, field):
 	for item in boxscore["gameBoxInfo"]:
 		if item["label"] == field:
-			return item["value"]
+			return item["value"][:-1]
 	return ""
+
+def process_final(finalObj, games):
+	print("SUMMARY:\n--------------------------------------\n")
+	print(f'Total players seen: {len(finalObj["players"])}')
+	process_stat(finalObj, "players", "Most seen players: ", get_player_name)
+	print(f'\nYou\'ve seen {len(finalObj["homeRuns"])} players hit {sum(finalObj["homeRuns"].values())} home runs')
+	process_stat(finalObj, "homeRuns", "Biggest power hitters: ", get_player_name)
+	print(f'\nYou\'ve seen {len(finalObj["triples"])} players hit {sum(finalObj["triples"].values())} triples')
+	process_stat(finalObj, "triples", "Fastest around the basepaths: ", get_player_name)
+
+	print(f'\nYou\'ve seen {games} games over the years!')
+	process_stat(finalObj, "attendance", "Most attended games: ", get_game_info)
+	process_stat(finalObj, "attendance", "Least attended games: ", get_game_info, False)
+	process_stat(finalObj, "gameTimes", "Longest games attended: ", get_game_info)
+	process_stat(finalObj, "gameTimes", "Shortest games attended: ", get_game_info, False)
+	
+
+def process_stat(finalObj, stat, label, process_item, most=True):
+	top = sorted(finalObj[stat].items(), reverse=most)
+	final_freq = top[4][1] if len(top) >= 5 else 0
+	print(label)
+	for item in top:
+		if item[1] < final_freq:
+			break
+		process_item(item, stat == "gameTimes")
+
+def get_player_name(player, _):
+	playerObj = statsapi.player_stat_data(player[0])
+	print(f"{playerObj['first_name']} {playerObj['last_name']}: {player[1]}")
+
+def get_game_info(game, isTime):
+	boxscore = statsapi.boxscore_data(game[0])
+	dateString = boxscore["gameBoxInfo"][-1]["label"]
+	away = boxscore["teamInfo"]["away"]["abbreviation"]
+	home = boxscore["teamInfo"]["home"]["abbreviation"]
+	value = game[1]
+	if isTime:
+		hour = int(game[1]/60)
+		minute = game[1] - 60 * hour
+		value = f"{hour}:{minute}"
+	print(f"{dateString} {away} vs. {home}: {value}")
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="MLB Analyzer")
